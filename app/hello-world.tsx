0066dc28b2904eb32d18bb36f96f7220d66730df
Tibberle2911 <tibber.le@gmail.com>
2025-09-07 19:17:59 +1000

    test

--- app/hello-world.tsx
+++ app/hello-world.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="justify-items-center">
-        <h1>Hello, World!</h1>
+        <h1>Tylor Le</h1>
         <Image
             src="/Logo.png"
             alt="Logo"
